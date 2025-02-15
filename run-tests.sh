8d8a2c1ce6c19a7f6b9ed49971e60e9664ef0f46
Marco Cutecchia <marco.cutecchia@outlook.it>
2025-02-16 00:33:36 +0100

    print status code on compile error

--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,6 @@ echo "Running tests..."
 for f in tests/*.S; do
     echo "Running test $f"
     arm-none-eabi-as $f -o $f.elf && arm-none-eabi-objcopy -O binary $f.elf $f.expected
-    ./charm $f $f.actual
+    ./charm $f $f.actual || echo "❌ Test $f failed: Charm returned status code $?"
     diff $f.actual $f.expected && echo "❎ Test $f passed" || >&2 echo "❌ Test $f failed"
 done
